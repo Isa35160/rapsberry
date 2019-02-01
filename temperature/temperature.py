9cec1d739ee96d47afa4c80f9420bdca1a2f4157
Altémir <a.barrier92@gmail.com>
2019-02-01 10:11:26 +0100

    temperature V.1.0.2

--- temperature/temperature.py
+++ temperature/temperature.py
@@ -12,31 +12,35 @@ device_file = '/sys/bus/w1/devices/28-01131a446afe/w1_slave'
 
 
 # Une fonction qui lit dans le fichier température
-def read_temp_raw():
-    f = open(device_file, 'r')  # Ouvre le dichier
-    lines = f.readlines()  # Returns the text
-    f.close()
-    return lines
+
+class TemperatureSensor:
+    def __init__(self, temperature):
+        self.temperature = temperature
+
+    def read_temp_raw(self):
+        f = open(device_file, 'r')  # Ouvre le fichier
+        lines = f.readlines()  # Returns the text
+        f.close()
+        return lines
+
+    def convert(self):
+        lines = self.read_temp_raw()  # Lit le fichier de température
+        # Tant que la première ligne ne vaut pas 'YES', on attend 0,2s
+        # On relis ensuite le fichier
+        while lines[0].strip()[-3:] != 'YES':
+            time.sleep(0.2)
+            lines = self.read_temp_raw()
+        # On cherche le '=' dans la seconde ligne du fichier
+        equals_pos = lines[1].find('t=')
+        # Si le '=' est trouvé, on converti ce qu'il y a après le '=' en degrées celcius
+        if equals_pos != -1:
+            temp_string = lines[1][equals_pos + 2:]
+            temp_c = float(temp_string) / 1000.0
+            print(temp_c)
 
 
-# Lis la temperature
-def read_temp():
-    lines = read_temp_raw()  # Lit le fichier de température
-    # Tant que la première ligne ne vaut pas 'YES', on attend 0,2s
-    # On relis ensuite le fichier
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
-        lines = read_temp_raw()
-    # On cherche le '=' dans la seconde ligne du fichier
-    equals_pos = lines[1].find('t=')
-    # Si le '=' est trouvé, on converti ce qu'il y a après le '=' en degrées celcius
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos + 2:]
-        temp_c = float(temp_string) / 1000.0
-        return temp_c
+TemperatureSensor.read_temp_raw()
+TemperatureSensor.convert()
 
 
-# On affiche la temérature tant que le script tourne
-while True:
-    print(read_temp())
-    time.sleep(1)
+
