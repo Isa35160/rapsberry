0f2bdb1b004d353bcd5493e6338096833982724a
Altémir <a.barrier92@gmail.com>
2019-02-01 14:10:37 +0100

    temperature V.1.1.2

--- temperature/temperature.py
+++ temperature/temperature.py
@@ -69,11 +69,12 @@ class TemperatureSensor:
             temp_string = lines[1][equals_pos + 2:]
             temp_c = float(temp_string) / 1000.0
             if temp_c < 21:
-                print('il fait froid, il fait actuellement', temp_c)
+                return 'il fait froid, il fait actuellement', temp_c
             elif 21 < temp_c < 21.5:
-                print('il fait bon il fait', temp_c)
+                return 'il fait bon il fait', temp_c
             else:
-                print('il fait chaud il fait', temp_c)
+                return 'il fait trop chaud :', temp_c
+
 
 tep = TemperatureSensor()
 # tep.read_temp_raw()
