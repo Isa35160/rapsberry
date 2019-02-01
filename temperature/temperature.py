19b78b1cfef9cb49523b72ea91ef39cc3942b85b
Altémir <a.barrier92@gmail.com>
2019-02-01 11:01:55 +0100

    temperature V.1.0.9

--- temperature/temperature.py
+++ temperature/temperature.py
@@ -25,7 +25,8 @@ class TemperatureSensor:
         return lines
 
     # Lis la temperature
-    def ConvertCelsius(self):
+    def Convert(self):
+        choice = str(input('Choose your degrees Celsius or Fahrenheit (C or F) : \n'))
         lines = self.read_temp_raw()  # Lit le fichier de température
         # Tant que la première ligne ne vaut pas 'YES', on attend 0,2s
         # On relis ensuite le fichier
@@ -35,31 +36,34 @@ class TemperatureSensor:
         # On cherche le '=' dans la seconde ligne du fichier
         equals_pos = lines[1].find('t=')
         # Si le '=' est trouvé, on converti ce qu'il y a après le '=' en degrées celcius
-        if equals_pos != -1:
+        if equals_pos != -1 and choice == 'C':
             temp_string = lines[1][equals_pos + 2:]
             temp_c = float(temp_string) / 1000.0
             print(temp_c, 'degrés celscius')
-
-    def ConvertFahrenheit(self):
-        lines = self.read_temp_raw()  # Lit le fichier de température
-        # Tant que la première ligne ne vaut pas 'YES', on attend 0,2s
-        # On relis ensuite le fichier
-        while lines[0].strip()[-3:] != 'YES':
-            time.sleep(0.2)
-            lines = self.read_temp_raw()
-        # On cherche le '=' dans la seconde ligne du fichier
-        equals_pos = lines[1].find('t=')
-        # Si le '=' est trouvé, on converti ce qu'il y a après le '=' en degrées celcius
-        if equals_pos != -1:
+        elif equals_pos != -1 and choice == 'F':
             temp_string = lines[1][equals_pos + 2:]
-            temp_f = float(temp_string) / 1000.0*9/5+32
+            temp_f = float(temp_string) / 1000.0 * 9 / 5 + 32
             print(temp_f, 'degrés fahrenheit')
+    #
+    # def ConvertFahrenheit(self):
+    #     lines = self.read_temp_raw()  # Lit le fichier de température
+    #     # Tant que la première ligne ne vaut pas 'YES', on attend 0,2s
+    #     # On relis ensuite le fichier
+    #     while lines[0].strip()[-3:] != 'YES':
+    #         time.sleep(0.2)
+    #         lines = self.read_temp_raw()
+    #     # On cherche le '=' dans la seconde ligne du fichier
+    #     equals_pos = lines[1].find('t=')
+    #     # Si le '=' est trouvé, on converti ce qu'il y a après le '=' en degrées celcius
+    #     if equals_pos != -1:
+    #         temp_string = lines[1][equals_pos + 2:]
+    #         temp_f = float(temp_string) / 1000.0*9/5+32
+    #         print(temp_f, 'degrés fahrenheit')
 
 
 tep= TemperatureSensor()
 tep.read_temp_raw()
-tep.ConvertCelsius()
-tep.ConvertFahrenheit()
+tep.Convert()
 
 
 
