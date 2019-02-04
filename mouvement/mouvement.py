d451356772de5d71991ff2c576752d92e5793424
Altémir <a.barrier92@gmail.com>
2019-02-04 10:22:58 +0100

    pull de la vérité 2

--- mouvement/mouvement.py
+++ mouvement/mouvement.py
@@ -3,33 +3,36 @@ import time
 
 # Initialisation de notre GPIO 17 pour recevoir un signal
 # Contrairement à nos LEDs avec lesquelles on envoyait un signal
-broche = 17
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(broche, GPIO.IN)
-GPIO.setup(18, GPIO.OUT)
-GPIO.setup(22, GPIO.OUT)
 
-currentstate = 0
-previousstate = 0
+class Mouvement():
+    broche = 17
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(broche, GPIO.IN)
+    GPIO.setup(18, GPIO.OUT)
+    GPIO.setup(22, GPIO.OUT)
 
-# Boucle infini jusqu'à CTRL-C
-while True:
-    # Lecture du capteur
-    currentstate = GPIO.input(broche)
-     # Si le capteur est déclenché
-    if currentstate == 1 and previousstate == 0:
-        GPIO.output(18, GPIO.HIGH)
-        GPIO.output(22, GPIO.HIGH)
-        time.sleep(1)
-        GPIO.output(18, GPIO.LOW)
-        GPIO.output(22, GPIO.LOW)
-        print("Mouvement détecté !")
-        # En enregistrer l'état
-        previousstate = 1
-    # Si le capteur est s'est stabilisé
-    elif currentstate == 0 and previousstate == 1:
-        print("    Prêt")
-
+    def detectMove(self):
+        currentstate = 0
         previousstate = 0
-    # On attends 10ms
+
+        while True:
+            # Lecture du capteur
+            currentstate = GPIO.input(self.broche)
+            # Si le capteur est déclenché
+            if currentstate == 1 and previousstate == 0:
+                GPIO.output(18, GPIO.HIGH)
+                GPIO.output(22, GPIO.HIGH)
+                time.sleep(1)
+                GPIO.output(18, GPIO.LOW)
+                GPIO.output(22, GPIO.LOW)
+                # print("Mouvement détecté !")
+                # En enregistrer l'état
+                previousstate = 1
+            # Si le capteur est s'est stabilisé
+            elif currentstate == 0 and previousstate == 1:
+                # print("Prêt")
+
+                previousstate = 0
+            # On attends 10ms
+
