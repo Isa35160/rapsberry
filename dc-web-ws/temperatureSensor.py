97a2ff1dfd0328c702a1337af050fe75d0e150d1
Altémir <a.barrier92@gmail.com>
2019-02-04 15:20:27 +0100

    ajout class tempsensor

--- dc-web-ws/temperatureSensor.py
+++ dc-web-ws/temperatureSensor.py
@@ -19,17 +19,19 @@ class TemperatureSensor:
 
     # Lis la temperature
     def readTempLive(self, socketIo):
-        lines = self.read_temp_raw()  # Lit le fichier de température
-        # Tant que la première ligne ne vaut pas 'YES', on attend 0,2s
-        # On relis ensuite le fichier
-        while lines[0].strip()[-3:] != 'YES':
-            time.sleep(0.2)
-            lines = self.read_temp_raw()
-        # On cherche le '=' dans la seconde ligne du fichier
-        equals_pos = lines[1].find('t=')
-        # Si le '=' est trouvé, on converti ce qu'il y a après le '=' en degrées celcius
-        if equals_pos != -1:
-            temp_string = lines[1][equals_pos + 2:]
-            temp_c = float(temp_string) / 1000.0
-            socketIo.emit('TempLive', temp_c, Broadcast=True)
+        while True:
+            lines = self.read_temp_raw()  # Lit le fichier de température
+            # Tant que la première ligne ne vaut pas 'YES', on attend 0,2s
+            # On relis ensuite le fichier
+            while lines[0].strip()[-3:] != 'YES':
+                time.sleep(0.2)
+                lines = self.read_temp_raw()
+            # On cherche le '=' dans la seconde ligne du fichier
+            equals_pos = lines[1].find('t=')
+            # Si le '=' est trouvé, on converti ce qu'il y a après le '=' en degrées celcius
+            if equals_pos != -1:
+                temp_string = lines[1][equals_pos + 2:]
+                temp_c = float(temp_string) / 1000.0
+                socketIo.emit('TempLive', temp_c, Broadcast=True)
+                time.sleep(1)
 
