cac2cf44189cf181f8bc870ccafddf567feac4e9
Altémir <a.barrier92@gmail.com>
2019-02-04 13:21:49 +0100

    pull de la vérité 5

--- dc-web-ws/mouvement.py
+++ dc-web-ws/mouvement.py
@@ -19,15 +19,16 @@ class Mouvement():
             currentstate = GPIO.input(self.broche)
             # Si le capteur est déclenché
             if currentstate == 1 and previousstate == 0:
+                GPIO.output(24 , GPIO.LOW)
                 GPIO.output(18, GPIO.HIGH)
-                time.sleep(5)
-                GPIO.output(18, GPIO.LOW)
+                time.sleep(2)
                 # En enregistrer l'état
                 previousstate = 1
             # Si le capteur est s'est stabilisé
             elif currentstate == 0 and previousstate == 1:
-                # print("Prêt")
-
+                GPIO.output(18, GPIO.LOW)
+                GPIO.output(24, GPIO.HIGH)
+                time.sleep(2)
                 previousstate = 0
             # On attends 10ms
 
